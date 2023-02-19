cd8c0d381df0a4325efa120149ce1bf5280ddfe6
chell <finz0mail@gmail.com>
2023-02-19 13:54:31 +0100

    bhop check

--- src/main/kotlin/me/chell/blackout/impl/features/movement/Bhop.kt
+++ src/main/kotlin/me/chell/blackout/impl/features/movement/Bhop.kt
@@ -5,6 +5,7 @@ import me.chell.blackout.api.events.PlayerTickEvent
 import me.chell.blackout.api.feature.Category
 import me.chell.blackout.api.feature.ToggleFeature
 import me.chell.blackout.api.util.eventManager
+import me.chell.blackout.api.util.mc
 import me.chell.blackout.api.util.player
 
 class Bhop: ToggleFeature("Bhop", Category.Movement, false) {
@@ -19,7 +20,7 @@ class Bhop: ToggleFeature("Bhop", Category.Movement, false) {
 
     @EventHandler
     fun onPlayerTick(event: PlayerTickEvent) {
-        if((player.forwardSpeed != 0f || player.sidewaysSpeed != 0f) && player.isOnGround && !player.isRiding && !player.isTouchingWater && !player.isSubmergedInWater)
+        if((player.forwardSpeed != 0f || player.sidewaysSpeed != 0f) && player.isOnGround && !mc.options.jumpKey.isPressed && !player.isRiding && !player.isTouchingWater && !player.isSubmergedInWater)
             player.jump()
     }
 
