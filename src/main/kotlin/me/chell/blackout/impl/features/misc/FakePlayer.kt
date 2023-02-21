b045b041acbbebaf1019a6fd4f94c50fdcf4f4d5
chell <finz0mail@gmail.com>
2023-02-21 12:34:36 +0100

    FakePlayer

---
+++ src/main/kotlin/me/chell/blackout/impl/features/misc/FakePlayer.kt
@@ -0,0 +1,33 @@
+package me.chell.blackout.impl.features.misc
+
+import com.mojang.authlib.GameProfile
+import me.chell.blackout.api.feature.Category
+import me.chell.blackout.api.feature.ToggleFeature
+import me.chell.blackout.api.util.mc
+import me.chell.blackout.api.util.player
+import net.minecraft.client.network.OtherClientPlayerEntity
+import net.minecraft.entity.Entity
+import java.util.*
+
+class FakePlayer: ToggleFeature("Fake Player", Category.Misc, false) {
+
+    private val uuid = UUID.randomUUID()
+    private var id = -1
+
+    override fun onEnable() {
+        val world = mc.world ?: return
+        mc.player ?: return
+
+        val entity = OtherClientPlayerEntity(world, GameProfile(uuid, "FakePlayer"))
+        entity.setPosition(player.pos)
+        entity.inventory.clone(player.inventory)
+
+        id = entity.id
+        world.addEntity(id, entity)
+    }
+
+    override fun onDisable() {
+        id = -1
+        mc.world?.removeEntity(id, Entity.RemovalReason.DISCARDED)
+    }
+}
--- src/main/kotlin/me/chell/blackout/impl/features/render/Fullbright.kt
+++ src/main/kotlin/me/chell/blackout/impl/features/render/Fullbright.kt
@@ -3,6 +3,7 @@ package me.chell.blackout.impl.features.render
 import me.chell.blackout.api.feature.Category
 import me.chell.blackout.api.feature.ToggleFeature
 
+/** @see me.chell.blackout.mixin.LightmapTextureManagerMixin */
 class Fullbright: ToggleFeature("Fullbright", Category.Render, false) {
 
     companion object {
