901cc983b54cc9d97cd2341a43c7ef17061758e7
chell <55377447+chell-dev@users.noreply.github.com>
2023-02-26 15:44:23 +0100

    PullDown

---
+++ src/main/kotlin/me/chell/blackout/impl/features/movement/PullDown.kt
@@ -0,0 +1,36 @@
+package me.chell.blackout.impl.features.movement
+
+import me.chell.blackout.api.event.EventHandler
+import me.chell.blackout.api.events.PlayerTickEvent
+import me.chell.blackout.api.feature.Category
+import me.chell.blackout.api.feature.ToggleFeature
+import me.chell.blackout.api.setting.Setting
+import me.chell.blackout.api.util.eventManager
+import me.chell.blackout.api.util.mc
+import me.chell.blackout.api.util.player
+import net.minecraft.world.RaycastContext
+
+class PullDown: ToggleFeature("PullDown", Category.Movement, false) {
+
+    private val maxHeight = register(Setting("Max Height", 4.0))
+    private val minHeight = register(Setting("Min Height", 0.1))
+
+    override fun onEnable() {
+        eventManager.register(this)
+    }
+
+    override fun onDisable() {
+        eventManager.unregister(this)
+    }
+
+    @EventHandler
+    fun onPlayerTick(event: PlayerTickEvent) {
+        if(!player.isOnGround) return
+        val ray = mc.world!!.raycast(RaycastContext(player.pos, player.pos.add(0.0, -maxHeight.value, 0.0), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.ANY, player))
+        val distance = player.pos.y - ray.pos.y
+        if(distance >= minHeight.value) {
+            player.upwardSpeed = -10f
+        }
+    }
+
+}
