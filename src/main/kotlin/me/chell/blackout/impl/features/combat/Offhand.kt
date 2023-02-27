bf0fe587da030f0248e4349ba7b00bed898f64dd
chell <55377447+chell-dev@users.noreply.github.com>
2023-02-27 18:00:34 +0100

    Fix Offhand

--- src/main/kotlin/me/chell/blackout/impl/features/combat/Offhand.kt
+++ src/main/kotlin/me/chell/blackout/impl/features/combat/Offhand.kt
@@ -6,8 +6,10 @@ import me.chell.blackout.api.feature.Category
 import me.chell.blackout.api.feature.ToggleFeature
 import me.chell.blackout.api.setting.Bind
 import me.chell.blackout.api.setting.Setting
+import me.chell.blackout.api.util.eventManager
 import me.chell.blackout.api.util.mc
 import me.chell.blackout.api.util.player
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen
 import net.minecraft.item.Items
 import net.minecraft.item.PickaxeItem
 import net.minecraft.item.SwordItem
@@ -22,38 +24,31 @@ class Offhand: ToggleFeature("Offhand", Category.Combat, false) {
 
     private val health = register(Setting("Totem HP", 16.0))
 
-    private var slot = -1
-    private var step = 1
+    override fun onEnable() {
+        eventManager.register(this)
+    }
+
+    override fun onDisable() {
+        eventManager.unregister(this)
+    }
 
     @EventHandler
     fun onPlayerTick(event: PlayerTickEvent) {
-        val item =
-            if(player.health + player.absorptionAmount <= health.value) Items.TOTEM_OF_UNDYING
+        val item = if(player.health + player.absorptionAmount <= health.value) Items.TOTEM_OF_UNDYING
         else if(gapple.value.enabled || (mc.options.useKey.isPressed
                 && ((swordGapple.value && player.mainHandStack.item is SwordItem) || (pickaxeGapple.value && player.mainHandStack.item is PickaxeItem))
                 )) Items.ENCHANTED_GOLDEN_APPLE
         else Items.END_CRYSTAL
 
-        if(player.offHandStack.item == item) return
+        if(player.offHandStack.item == item || mc.currentScreen is AbstractInventoryScreen<*>) return
 
-        when(step) {
-            1 -> {
-                for(i in 0 until player.inventory.main.size) {
-                    if(player.inventory.getStack(slot).item == item) {
-                        mc.interactionManager!!.clickSlot(player.currentScreenHandler.syncId, i, 0, SlotActionType.PICKUP_ALL, player)
-                        slot = i
-                        step++
-                        break
-                    }
-                }
-            }
-            2 -> {
-                mc.interactionManager!!.clickSlot(player.currentScreenHandler.syncId, 45, 0, SlotActionType.PICKUP_ALL, player)
-                step++
-            }
-            3 -> {
-                mc.interactionManager!!.clickSlot(player.currentScreenHandler.syncId, slot, 0, SlotActionType.PICKUP_ALL, player)
-                step = 1
+        for(i in 0 until player.inventory.main.size) {
+            if(player.inventory.getStack(i).item == item) {
+                val slot = if(i < 9) i + 36 else i
+                mc.interactionManager!!.clickSlot(player.currentScreenHandler.syncId, slot, 0, SlotActionType.PICKUP, player)
+                mc.interactionManager!!.clickSlot(player.currentScreenHandler.syncId, 45, 0, SlotActionType.PICKUP, player)
+                mc.interactionManager!!.clickSlot(player.currentScreenHandler.syncId, slot, 0, SlotActionType.PICKUP, player)
+                break
             }
         }
     }
