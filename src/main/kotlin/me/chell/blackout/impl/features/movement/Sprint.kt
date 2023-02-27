ed6be2f0fdbfff9744a07b093151f179a5f6d188
chell <55377447+chell-dev@users.noreply.github.com>
2023-02-27 18:44:26 +0100

    Remove sprint speed

--- src/main/kotlin/me/chell/blackout/impl/features/movement/Sprint.kt
+++ src/main/kotlin/me/chell/blackout/impl/features/movement/Sprint.kt
@@ -1,7 +1,6 @@
 package me.chell.blackout.impl.features.movement
 
 import me.chell.blackout.api.event.EventHandler
-import me.chell.blackout.api.events.InputEvent
 import me.chell.blackout.api.events.PlayerTickEvent
 import me.chell.blackout.api.feature.Category
 import me.chell.blackout.api.feature.Feature
@@ -9,15 +8,13 @@ import me.chell.blackout.api.setting.Bind
 import me.chell.blackout.api.util.eventManager
 import me.chell.blackout.api.util.player
 import me.chell.blackout.api.setting.Setting
-import me.chell.blackout.api.util.mc
 import net.minecraft.entity.effect.StatusEffects
-import net.minecraft.text.Text
 
 class Sprint: Feature("Sprint", Category.Movement) {
 
     override val mainSetting = Setting("Enabled", Bind.Toggle(onEnable = { onEnable() }, onDisable = { onDisable() }))
 
-    private val mode = register(Setting("Mode", Mode.Speed))
+    private val mode = register(Setting("Mode", Mode.Rage))
 
     private fun onEnable() {
         eventManager.register(this)
@@ -29,7 +26,6 @@ class Sprint: Feature("Sprint", Category.Movement) {
 
     @EventHandler
     fun onPlayerTick(event: PlayerTickEvent) {
-        // todo use 'when'
         if(mode.value == Mode.Legit) {
             if((player.hungerManager.foodLevel > 6 || player.abilities.allowFlying) &&
                     player.forwardSpeed > 0f
@@ -39,39 +35,11 @@ class Sprint: Feature("Sprint", Category.Movement) {
                 player.isSprinting = true
         } else if(mode.value == Mode.Rage) {
             if(player.forwardSpeed != 0f || player.sidewaysSpeed != 0f) player.isSprinting = true
-        } else if(mode.value == Mode.Speed) {
-            if(mc.options.forwardKey.isPressed) {
-                player.forwardSpeed = 1f
-                player.isSprinting = true
-                player.airStrafingSpeed = 1f
-            } else if(mc.options.backKey.isPressed) {
-                player.forwardSpeed = -1f
-                player.isSprinting = true
-                player.airStrafingSpeed = 1f
-            } else {
-                player.forwardSpeed = 0f
-                player.isSprinting = false
-                player.airStrafingSpeed = 0.02f
-            }
-
-            if(mc.options.rightKey.isPressed) {
-                player.sidewaysSpeed = 1f
-                player.isSprinting = true
-                player.airStrafingSpeed = 1f
-            } else if(mc.options.leftKey.isPressed) {
-                player.sidewaysSpeed = -1f
-                player.isSprinting = true
-                player.airStrafingSpeed = 1f
-            } else {
-                player.sidewaysSpeed = 0f
-                player.isSprinting = false
-                player.airStrafingSpeed = 0.02f
-            }
         }
     }
 
     enum class Mode {
-        Legit, Rage, Speed
+        Legit, Rage
     }
 
 }
