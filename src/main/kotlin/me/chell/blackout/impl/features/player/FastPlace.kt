d7c3d99070a548dce4d59e49803c7343a9e030f9
chell <55377447+chell-dev@users.noreply.github.com>
2023-03-11 12:52:59 +0100

    FastPlace

---
+++ src/main/kotlin/me/chell/blackout/impl/features/player/FastPlace.kt
@@ -0,0 +1,28 @@
+package me.chell.blackout.impl.features.player
+
+import me.chell.blackout.api.event.EventHandler
+import me.chell.blackout.api.events.PlayerTickEvent
+import me.chell.blackout.api.feature.Category
+import me.chell.blackout.api.feature.ToggleFeature
+import me.chell.blackout.api.util.eventManager
+import me.chell.blackout.api.util.mc
+import me.chell.blackout.mixin.accessors.MinecraftClientAccessor
+
+class FastPlace: ToggleFeature("FastPlace", Category.PLayer, false) {
+
+    private val accessor = mc as MinecraftClientAccessor
+
+    override fun onEnable() {
+        eventManager.register(this)
+    }
+
+    override fun onDisable() {
+        eventManager.unregister(this)
+    }
+
+    @EventHandler
+    fun onTick(event: PlayerTickEvent) {
+        accessor.itemUseCooldown = 0
+    }
+
+}
