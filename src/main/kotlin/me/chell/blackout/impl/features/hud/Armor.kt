f23bf8134f239ec2142d62154f33a89d66b9edf3
chell <55377447+chell-dev@users.noreply.github.com>
2023-03-11 13:48:25 +0100

    Armor widget

---
+++ src/main/kotlin/me/chell/blackout/impl/features/hud/Armor.kt
@@ -0,0 +1,24 @@
+package me.chell.blackout.impl.features.hud
+
+import me.chell.blackout.api.feature.Widget
+import me.chell.blackout.api.util.mc
+import me.chell.blackout.api.util.player
+import me.chell.blackout.api.util.textRenderer
+import net.minecraft.client.util.math.MatrixStack
+
+class Armor: Widget("Armor") {
+
+    override var width = 64
+    override var height = 16
+
+    override fun render(matrices: MatrixStack?, mouseX: Int, mouseY: Int, delta: Float) {
+        super.render(matrices, mouseX, mouseY, delta)
+
+        var itemX = x.value
+        for(stack in player.armorItems.reversed()) {
+            mc.itemRenderer.renderInGui(stack, itemX, y.value)
+            mc.itemRenderer.renderGuiItemOverlay(textRenderer, stack, itemX, y.value)
+            itemX += 16
+        }
+    }
+}
