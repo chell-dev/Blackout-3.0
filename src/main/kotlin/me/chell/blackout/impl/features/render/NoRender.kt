d248d2c563085bae3b1e164e39199bcd4863860e
chell <55377447+chell-dev@users.noreply.github.com>
2023-05-07 15:43:27 +0200

    NoRender gui stuff

--- src/main/kotlin/me/chell/blackout/impl/features/render/NoRender.kt
+++ src/main/kotlin/me/chell/blackout/impl/features/render/NoRender.kt
@@ -6,6 +6,7 @@ import me.chell.blackout.api.events.RenderHudEvent
 import me.chell.blackout.api.feature.Category
 import me.chell.blackout.api.feature.ToggleFeature
 import me.chell.blackout.api.setting.Setting
+import me.chell.blackout.api.util.player
 import net.minecraft.client.particle.ExplosionLargeParticle
 import net.minecraft.client.particle.ExplosionSmokeParticle
 import net.minecraft.client.particle.FireworksSparkParticle.FireworkParticle
@@ -14,25 +15,31 @@ import net.minecraft.client.particle.TotemParticle
 
 object NoRender: ToggleFeature("NoRender", Category.Render) {
 
-    private val explosions = register(Setting("Explosion Particles", false))
-    private val potionParticles = register(Setting("Potion Particles", false))
-    private val totemParticles = register(Setting("Totem Particles", false))
-    private val fireworks = register(Setting("Firework Particles", false))
+    private val particles = register(Setting("Particles", null))
+    private val explosions = register(Setting("Explosion", false, level = 2))
+    private val potionParticles = register(Setting("Potion", false, level = 2))
+    private val onlyOwnPotion = register(Setting("Only Self", false, level = 3) { potionParticles.value})
+    private val totemParticles = register(Setting("Totem", false, level = 2))
+    private val fireworks = register(Setting("Firework", false, level = 2))
     private val hurtCam = register(Setting("Hurt Camera", false))
-    private val fire = register(Setting("Fire Overlay", false))
-    private val pumpkin = register(Setting("Pumpkin Overlay", false))
-    private val snowOverlay = register(Setting("Snow Overlay", false))
-    private val inWall = register(Setting("In Wall Overlay", false))
-    private val portalOverlay = register(Setting("Portal Overlay", false))
-    private val waterOverlay = register(Setting("Underwater Overlay", false))
-    private val totemOverlay = register(Setting("Totem Overlay", false))
+    private val overlays = register(Setting("Overlays", null))
+    private val fire = register(Setting("Fire", false, level = 2))
+    private val pumpkin = register(Setting("Pumpkin", false, level = 2))
+    private val snowOverlay = register(Setting("Snow", false, level = 2))
+    private val inWall = register(Setting("In Wall", false, level = 2))
+    private val portalOverlay = register(Setting("Portal", false, level = 2))
+    private val waterOverlay = register(Setting("Underwater", false, level = 2))
+    private val totemOverlay = register(Setting("Totem", false, level = 2))
     private val tooltip = register(Setting("Item Tooltip", false))
 
     @EventHandler
     fun onParticle(event: ParticleEvent) {
         when(event.particle) {
             is ExplosionLargeParticle, is ExplosionSmokeParticle -> if(explosions.value) event.canceled = true
-            is SpellParticle -> if(potionParticles.value) event.canceled = true
+            is SpellParticle -> if(potionParticles.value) {
+                if(!onlyOwnPotion.value || player.squaredDistanceTo(event.particle.boundingBox.center) <= 4.0)
+                    event.canceled = true
+            }
             is TotemParticle -> if(totemParticles.value) event.canceled = true
             is FireworkParticle -> if(fireworks.value) event.canceled = true
         }
